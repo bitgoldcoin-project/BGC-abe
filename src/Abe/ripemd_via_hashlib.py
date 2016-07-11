84c52174af2fff6a536f1c0d01024648387876cd
odinsoft <odinsoft@odinsoftware.co.kr>
2016-07-11 18:33:44 +0900

    merge with local source

---
+++ src/Abe/ripemd_via_hashlib.py
@@ -0,0 +1,9 @@
+﻿# RIPEMD hash interface via hashlib for those who don't have
+# Crypto.Hash.RIPEMD.
+
+import hashlib
+
+def new(data=''):
+    h = hashlib.new('ripemd160')
+    h.update(data)
+    return h
